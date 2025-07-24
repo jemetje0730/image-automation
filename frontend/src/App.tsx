518372ee2d8a6762e271738d2590e45e3132945a
jemetje0730 <jemetje0730@gmail.com>
2025-07-24 15:18:51 +0900

    chore: setup initial App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import FlowEditor from './components/editors/FlowEditor'
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <FlowEditor />
+    </div>
+  )
+}
+
+export default App
